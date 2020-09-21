a5e6343a809d1c2d77c066c0a15cb03654b29d07
jakub <plutakuba@gmail.com>
2020-09-21 21:17:35 +0200

    Sample code with signal bars

---
+++ pytrader/prices.py
@@ -0,0 +1,87 @@
+import alpaca_trade_api as alpaca
+from pytrader.trader import PyTrader
+import time
+import datetime
+from datetime import timedelta
+from pytz import timezone
+from configparser import ConfigParser
+
+tz = timezone("Europe/Warsaw")
+
+import logging
+logging.basicConfig(filename='./apca_algo.log', format='%(name)s - %(levelname)s - %(message)s')
+logging.warning('{} logging started'.format(datetime.datetime.now().strftime("%x %X")))
+
+config = ConfigParser()
+config.read(r'C:\Repository\priv\trader\config\config.ini')
+
+KEY = config.get('api', 'KEY')
+SECRET = config.get('api', 'SECRET')
+BASE = config.get('api', 'BASE')
+
+
+pyt = PyTrader(KEY, SECRET, BASE)
+
+def get_data_bars(symbols, timeframe='5Min',  slow=30, fast=5):
+    data = pyt.client.get_barset(symbols, timeframe, limit=20).df
+    for symbol in symbols:
+        data.loc[:, (symbol, 'fast_ema')] = data[symbol]['close'].rolling(window=fast).mean()
+        data.loc[:, (symbol, 'slow_ema')] = data[symbol]['close'].rolling(window=slow).mean()
+    return data
+
+def get_signal_bars(symbol_list, rate, ema_slow, ema_fast):
+    data = pyt.client.get_data_bars(symbol_list, rate, ema_slow, ema_fast)
+    signals = {}
+    for x in symbol_list:
+        if data[x].iloc[-1]['fast_ema'] > data[x].iloc[-1]['slow_ema']: signal = 1
+        else: signal = 0
+        signals[x] = signal
+    return signals
+
+def time_to_open(current_time):
+    if current_time.weekday() <= 4:
+        d = (current_time + timedelta(days=1)).date()
+    else:
+        days_to_mon = 0 - current_time.weekday() + 7
+        d = (current_time + timedelta(days=days_to_mon)).date()
+    next_day = datetime.datetime.combine(d, datetime.time(9, 30, ))
+    seconds = (next_day - current_time).total_seconds()
+    return seconds
+
+
+def run(stocks):
+
+    while True:
+        if 0 <= datetime.datetime.now().weekday() <= 4:
+            print("Trading Session!")
+            if datetime.time(9, 30) < datetime.datetime.now().time() <= datetime.time(15, 30):
+                signals = get_signal_bars(stocks, '5Min', 20, 5)
+                for signal in signals:
+                    if signals[signal] == 1:
+                        if signal not in [x.symbol for x in api.list_positions()]:
+                            logging.warning('{} {} - {}'.format(datetime.datetime.now().strftime("%x %X"), signal,
+                                                                signals[signal]))
+                            pyt.client.submit_order(signal, 1, 'buy', 'market', 'day')
+                    else:
+                        try:
+                            pyt.client.submit_order(signal, 1, 'sell', 'market', 'day')
+                            logging.warning('{} {} - {}'.format(datetime.datetime.now().strftime("%x %X"), signal,
+                                                                signals[signal]))
+                        except Exception as e:
+                            pass
+                time.sleep(60)
+            else:
+                # Get time amount until open, sleep that amount
+                print('Market closed ({})'.format(datetime.datetime.now()))
+                print('Sleeping', round(time_to_open(datetime.datetime.now())/60/60, 2), 'hours')
+                time.sleep(time_to_open(datetime.datetime.now()))
+
+
+
+stocks = ['AAPL', 'FB', 'AT', 'CSCO',"BEN", "HPE", "IBM",'AMD','MO']
+
+print('test:')
+print(get_data_bars(['AA'], '5Min', 20, 5).head())
+
+run(stocks)
+
